47f5e6284afd8cc581a76875ad7ddce60667611a
Thairam01 <thayram01@gmail.com>
2019-07-24 21:37:10 -0300

    consertando bugs

--- Códigos - URI/2018/Altura URI - 1566.cpp
+++ Códigos - URI/2018/Altura URI - 1566.cpp
@@ -1,11 +1,9 @@
 #include <bits/stdc++.h>
-#define debug(x) cout << #x " = " << (x) << endl
-#define endl '\n'
 
 using namespace std;
 
 int main() {
-  ios_base::sync_with_stdio(false);cin.tie(NULL);
+  
   int tc; cin >> tc;
   while (tc--) {
     int n, t; cin >> n;
